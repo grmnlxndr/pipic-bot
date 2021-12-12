4096b3b10579b94c4a78be9de5127b68993dce8b
German Schacht <schacht.german@gmail.com>
2021-12-11 23:17:02 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+
+const { Telegraf } = require('telegraf');
+const NodeWebcam = require('node-webcam');
+
+const webcamOptions = {
+  width: 640,
+  height: 480,
+  quality: 100,
+  delay: 0,
+  saveShots: true,
+  output: 'jpeg',
+  device: false,
+  callbackReturn: 'location',
+  location: '/images',
+  verbose: false
+};
+
+const bot = new Telegraf(process.env.BOT_API_KEY);
+const allowedIds = process.env.USER_IDS.split(',');
+
+bot.command('start', (ctx) => {
+  console.log(ctx.from);
+  bot.telegram.sendMessage(ctx.chat.id, 'Hello there! Welcome!');
+});
+
+bot.command('pic', (ctx) => {
+  console.log(ctx.from);
+  if (allowedIds.includes(ctx.from.id.toString())) {
+    bot.telegram.sendMessage(ctx.chat.id, `Hello ${ctx.from.first_name}! Taking photo, one moment please...`);
+    NodeWebcam.capture(
+      `images/pic_${Date.now()}`,
+      webcamOptions,
+      (err, data) => {
+        console.log(`Sending photo ${data}`);
+        bot.telegram.sendPhoto(ctx.chat.id, { source: data });
+      }
+    );
+  } else {
+    bot.telegram.sendMessage(ctx.chat.id, 'Sorry, not allowed!');
+  }
+});
+
+bot.launch();
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
